6f4909e72db220ec34b20c7538161e6ffd65e05d
Ahmad Faruqi <ahmadfaruqi@Ahmads-MacBook-Pro.local>
2023-11-10 17:22:46 -0500

    initial wire up of GarageDoor Service/characteristics

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,26 +1,25 @@
 import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
 
-import { ExampleHomebridgePlatform } from './platform';
+import { UniversalGarageDoorPlatform } from './platform';
 
 /**
  * Platform Accessory
  * An instance of this class is created for each accessory your platform registers
  * Each accessory may expose multiple services of different service types.
  */
-export class ExamplePlatformAccessory {
+export class UniversalGarageDoorPlatformAccessory {
   private service: Service;
+  private currentDoorState = this.platform.Characteristic.CurrentDoorState.CLOSED;
+  private doorTargetState = this.doorTargetStateBias(this.currentDoorState);
 
   /**
    * These are just used to create a working example
    * You should implement your own code to track the state of your accessory
    */
-  private exampleStates = {
-    On: false,
-    Brightness: 100,
-  };
+
 
   constructor(
-    private readonly platform: ExampleHomebridgePlatform,
+    private readonly platform: UniversalGarageDoorPlatform,
     private readonly accessory: PlatformAccessory,
   ) {
 
@@ -30,25 +29,27 @@ export class ExamplePlatformAccessory {
       .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
       .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
 
-    // get the LightBulb service if it exists, otherwise create a new LightBulb service
-    // you can create multiple services for each accessory
-    this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
+    // get the Garage Door service if it exists, otherwise create a new Garage Door service
+    this.service = this.accessory.getService(this.platform.Service.GarageDoorOpener) ||
+    this.accessory.addService(this.platform.Service.GarageDoorOpener);
 
     // set the service name, this is what is displayed as the default name on the Home app
     // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
-    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.exampleDisplayName);
+    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.exampleDisplayName); //update this...
 
     // each service must implement at-minimum the "required characteristics" for the given service type
     // see https://developers.homebridge.io/#/service/Lightbulb
+    //*******************************************************************************//
+    //****************************REGISTER HANDLERS**********************************//
+    //*******************************************************************************//
+    // register handlers for the CurrentDoorState Characteristic
+    this.service.getCharacteristic(this.platform.Characteristic.CurrentDoorState);
+    // register handlers for the TargetDoorState Characteristic
+    //.onGet(this.getOn.bind(this));               // GET - bind to the `getOn` method below
+    this.service.getCharacteristic(this.platform.Characteristic.TargetDoorState);
+    // register handlers for the ObstructionDetected Characteristic
+    this.service.getCharacteristic(this.platform.Characteristic.ObstructionDetected);
 
-    // register handlers for the On/Off Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.On)
-      .onSet(this.setOn.bind(this))                // SET - bind to the `setOn` method below
-      .onGet(this.getOn.bind(this));               // GET - bind to the `getOn` method below
-
-    // register handlers for the Brightness Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-      .onSet(this.setBrightness.bind(this));       // SET - bind to the 'setBrightness` method below
 
     /**
      * Creating multiple services of the same type.
@@ -95,12 +96,12 @@ export class ExamplePlatformAccessory {
    * Handle "SET" requests from HomeKit
    * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
    */
-  async setOn(value: CharacteristicValue) {
-    // implement your own code to turn your device on/off
-    this.exampleStates.On = value as boolean;
+  // async setOn(value: CharacteristicValue) {
+  //   // implement your own code to turn your device on/off
+  //   this.exampleStates.On = value as boolean;
 
-    this.platform.log.debug('Set Characteristic On ->', value);
-  }
+  //   this.platform.log.debug('Set Characteristic On ->', value);
+  // }
 
   /**
    * Handle the "GET" requests from HomeKit
@@ -115,27 +116,48 @@ export class ExamplePlatformAccessory {
    * @example
    * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
    */
-  async getOn(): Promise<CharacteristicValue> {
-    // implement your own code to check if the device is on
-    const isOn = this.exampleStates.On;
+  async getCurrentDoorState(): Promise<CharacteristicValue> {
+    const currentState = this.currentDoorState;
 
-    this.platform.log.debug('Get Characteristic On ->', isOn);
+    this.platform.log.debug('Get Characteristic On ->', currentState);
 
     // if you need to return an error to show the device as "Not Responding" in the Home app:
     // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
 
-    return isOn;
+    return currentState;
   }
+
+
+  // async getCurrentDoorState(): Promise<CharacteristicValue> {
+
+  // }
 
   /**
    * Handle "SET" requests from HomeKit
    * These are sent when the user changes the state of an accessory, for example, changing the Brightness
    */
-  async setBrightness(value: CharacteristicValue) {
-    // implement your own code to set the brightness
-    this.exampleStates.Brightness = value as number;
+  // async setBrightness(value: CharacteristicValue) {
+  //   // implement your own code to set the brightness
+  //   this.exampleStates.Brightness = value as number;
 
-    this.platform.log.debug('Set Characteristic Brightness -> ', value);
+  //   this.platform.log.debug('Set Characteristic Brightness -> ', value);
+  // }
+
+  private doorTargetStateBias(doorState: CharacteristicValue): CharacteristicValue {
+
+    switch(doorState) {
+
+      case this.platform.Characteristic.CurrentDoorState.OPEN:
+      case this.platform.Characteristic.CurrentDoorState.OPENING:
+      case this.platform.Characteristic.CurrentDoorState.STOPPED:
+        return this.platform.Characteristic.TargetDoorState.OPEN;
+        break;
+
+      case this.platform.Characteristic.CurrentDoorState.CLOSED:
+      case this.platform.Characteristic.CurrentDoorState.CLOSING:
+      default:
+        return this.platform.Characteristic.TargetDoorState.CLOSED;
+        break;
+    }
   }
-
 }

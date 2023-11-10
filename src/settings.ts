9c392e258e494cc496d088cb7b59a6371c67b438
Ahmad Faruqi <ahmadfaruqi@Ahmads-MacBook-Pro.local>
2023-11-10 00:45:16 -0500

    initial setup

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,21 @@
-/**
- * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
- */
-export const PLATFORM_NAME = 'ExampleHomebridgePlugin';
 
-/**
- * This must match the name of your plugin as defined the package.json
- */
-export const PLUGIN_NAME = 'homebridge-plugin-name';
+//**********************************************************************//
+//---------------------------Time Intervals(s)--------------------------//
+//**********************************************************************//
+
+export const DEVICE_REFRESH_INTERVAL = 5; // for polling the Garage Door controller API for updates about device states.
+
+
+export const ACTIVE_DEVICE_REFRESH_INTERVAL = 1; // for polling the controller controller API during active state change.
+
+
+export const ACTIVE_DEVICE_REFRESH_DURATION = 60 * 5; //for how long we should continue to actively poll device state changes.
+
+//**********************************************************************//
+//--------------------------Plugin Configuration------------------------//
+//**********************************************************************//
+
+export const PLATFORM_NAME = 'UniversalGdo'; // the name of the platform created by the plugin.
+
+
+export const PLUGIN_NAME = 'homebridge-universal-gdo'; // The name of our plugin.
